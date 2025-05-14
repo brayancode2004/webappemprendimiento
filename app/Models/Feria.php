70ddbe6c31cdb407512e22a5da1a2ca93a9c243d
BrunoJMA <bjmejia@uamv.edu.ni>
2025-05-13 22:55:05 -0600

    Definicion de los modelos

--- app/Models/Feria.php
+++ app/Models/Feria.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feria extends Model
 {
-    //
+    protected $fillable = [
+        'nombre',
+        'fecha',
+        'lugar',
+        'descripcion',
+    ];
+
+    public function emprendedores()
+    {
+        return $this->belongsToMany(Emprendedor::class);
+    }
 }
